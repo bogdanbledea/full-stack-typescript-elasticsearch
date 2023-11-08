ff89850b98249ae57bd081faf9476a31f9f87a4c
Bogdan Bledea <bledeabogdanalexandru@gmail.com>
2023-11-08 15:57:36 +0200

    Setup winston logging for backend

--- backend/src/Server.ts
+++ backend/src/Server.ts
@@ -1,14 +1,31 @@
 import express, { Express, Request, Response , Application } from 'express';
 import dotenv from 'dotenv';
+import winston from 'winston';
+import ecsFormat from '@elastic/ecs-winston-format'
 
 //For env File 
 dotenv.config();
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: ecsFormat(),
+  transports: [new winston.transports.Console()],
+});
 
 const app: Application = express();
 const port = process.env.PORT || 8000;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to Express & TypeScript Server');
+});
+
+app.get('/posts', (req: Request, res: Response) => {
+  logger.info('get posts from user uid345')
+  res.send('Get posts successfully');
+});
+
+app.get('/videos', (req: Request, res: Response) => {
+  res.send('Get videos successfully');
 });
 
 app.listen(port, () => {
